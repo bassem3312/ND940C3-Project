e3fa17fbf8227abd544126fe33edb3ad67c32c55
Bassem Mohsen <basem3312@gmail.com>
2023-01-17 21:52:48 +0200

    Update MainActivity.kt
    commit

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var downloadManager: DownloadManager
     private var downloadID: Long = 0
 
+    //Bassem mohsen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
