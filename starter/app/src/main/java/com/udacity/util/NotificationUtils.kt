75ae1a778e80a56a4dac907dd77e085e33285128
bassem3312 <basem3312@gmail.com>
2023-01-17 21:40:47 +0200

    Commit after update branch 32

--- starter/app/src/main/java/com/udacity/util/NotificationUtils.kt
+++ starter/app/src/main/java/com/udacity/util/NotificationUtils.kt
@@ -28,7 +28,7 @@ fun NotificationManager.sendNotification(messageBody: String, status: String, ap
     contentIntent.putExtra(EXTRA_DOWNLOAD_STATUS, status)
     val pendingIntent: PendingIntent = PendingIntent.getActivity(appContext, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-    //Bassem Mohsen 2nd branch 
+    //Bassem Mohsen 2nd branch 33
 
     val downloadImage = BitmapFactory.decodeResource(
         appContext.resources,
